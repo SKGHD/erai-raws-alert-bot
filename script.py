2a5f80440eb124999d0d1871d415666c7a85771d
SKGHD <sangramgorai@gmail.com>
2020-11-23 20:59:55 +0530

    Refactored and added comments

--- script.py
+++ script.py
@@ -6,6 +6,7 @@ from pymongo import MongoClient
 from datetime import datetime
 from pytz import timezone
 from os import environ as env
+import shortuuid
 
 
 ############## aria2 configuration ################
@@ -23,22 +24,33 @@ bot_chatID = env.get("TELEGRAM_CHAT_ID")
 counter = 1
 SERVER_START_TIME = datetime.now(timezone('Asia/Kolkata'))
 
+### Creating Connection to Database server ###
+cluster = MongoClient(env.get("MONGO_DRIVER_KEY"))
+db = cluster["horriblesubsbot"]
+
+
 # fetching data from mongo server
-
-
 def fetchData(collectionName):
-    cluster = MongoClient(env.get("MONGO_DRIVER_KEY"))
-    db = cluster["horriblesubsbot"]
     collection = db[collectionName]
     return collection
 
 
+# storing fetched data to a list
 def storeData(collection):
     documentList = []
     results = collection.find({})
     for result in results:
         documentList.append(result["name"])
     return documentList
+
+# Sending msg to telegram bot
+
+
+def sendMsg(episodeName):
+    msg = f"New episode available = {episodeName} "
+    send_text = 'https://api.telegram.org/bot' + bot_token + \
+        '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=HTML&text=' + msg
+    requests.get(send_text)
 
 
 while True:
@@ -53,41 +65,33 @@ while True:
         namesList.append(getList.entries[i].title)
         magnetList.append(getList.entries[i].link)
 
-    def sendMsg(k):
-        msg = f"New episode available = {namesList[k]} "
-        send_text = 'https://api.telegram.org/bot' + bot_token + \
-            '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=HTML&text=' + msg
-        requests.get(send_text)
-        # Adding to my Download tasks
-        aria2.add_magnet(magnetList[k])
-
-    def getLastID():
-        lastID = fetchData("completedjobs").count_documents({})
-        return(lastID + 1)
-
-    def updateContent():
-        # get all name: key values and store them in a list called completedJobs then return (completedJobs)
-        completedJobs = storeData(fetchData("completedjobs"))
-        return (completedJobs)
+    completedJobsCollection = fetchData("completedjobs")
 
     def updateDB(newItem):
         # post new update to database
         updateTime = currentTime.strftime("%d-%m-%y %I:%M:%S %p")
-        post = {"_id": getLastID(), "name": newItem, "date": updateTime}
-        fetchData("completedjobs").insert_one(post)
-        print(f'Added to the database "completedjobs" = {newItem}')
+        post = {"_id": shortuuid.uuid(), "name": newItem, "date": updateTime}
+        completedJobsCollection.insert_one(post)
+        print(f'Added to the collection "completedjobs" = {newItem}')
 
     for i in animeList:
         print(f'Searching for {i} in RSS feed')
         if str(i) in str(namesList):
             # index of namesList item which matched with animeList
             k = [idx for idx, s in enumerate(namesList) if i in s][0]
-            completedJobscontent = updateContent()
-            if (namesList[k] in str(completedJobscontent)) == False:
+
+            # Updating completedJobsList with latest documents
+            completedJobsList = storeData(completedJobsCollection)
+            # If an episode is not already in the list then execute these tasks
+            if (namesList[k] in str(completedJobsList)) == False:
                 print(f'Found new Episode: {namesList[k]}')
                 updateDB(namesList[k])
-                sendMsg(k)
-    ###################Bunch of STDOUTS################################
+                sendMsg(namesList[k])
+                # Adding to my Download tasks
+                aria2.add_magnet(magnetList[k])
+
+###################Bunch of STDOUTS################################
+
     print(f'Server running since {SERVER_START_TIME}')
     print(f"Current Time : {datetime.now(timezone('Asia/Kolkata'))}")
     print(f'I have looped for {counter} times since server start')
