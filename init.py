8c8bbd25a45e2401eccc02c2eac66da0eee96051
SKGHD <sangramgorai@gmail.com>
2020-11-26 04:08:50 +0530

    add form added

--- init.py
+++ init.py
@@ -1,14 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from handlers import fetchData, storeData
+from datetime import datetime
+from pytz import timezone
 
 app = Flask(__name__)
 
 
+animeListContent = fetchData('animelist')
+
+
+def loadAnimeList():
+    global animeList
+    animeList = storeData(animeListContent)
+
+
+loadAnimeList()
+
+
 @app.route('/')
 def index():
-    animeList = storeData(fetchData('animelist'))
-    return render_template('index.html', mylist=animeList)
+
+    addNew = request.args.get('nameInput')
+    if addNew != None:
+        animeListContent.insert_one(
+            {"name": addNew, "timestamp": datetime.now(timezone('Asia/Kolkata'))})
+        loadAnimeList()
+
+    return render_template('index.html', mylist=animeList, anime=addNew)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
